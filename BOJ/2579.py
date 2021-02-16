aa79dd18e61a6c8733c98071e2b6606ad70d6219
ORANZINO <hupajupa61@gmail.com>
2021-02-16 14:52:56 +0900

    2021/02/16

---
+++ BOJ/2579.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+ans = [0 for _ in range(n)]
+for _ in range(n):
+    arr.append(int(input()))
+
+ans[0] = arr[0]
+if n > 1:
+    ans[1] = arr[0] + arr[1]
+if n > 2:
+    ans[2] = max(arr[1] + arr[2], arr[0] + arr[2])
+
+for i in range(3, n):
+    ans[i] = max(ans[i - 2] + arr[i], ans[i - 3] + arr[i - 1] + arr[i])
+
+print(ans[n - 1])
