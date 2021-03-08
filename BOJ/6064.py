6a493bd657a416b995f8db7bd65517b205d2a94d
ORANZINO <hupajupa61@gmail.com>
2021-03-08 21:55:12 +0900

    2021/03/08

--- BOJ/6064.py
+++ BOJ/6064.py
@@ -5,6 +5,9 @@ input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
     m, n, x, y = map(int, input().split())
+    if n > m:
+        m, n = n, m
+        x, y = y, x
     k = x
     y = 0 if n == y else y
     end = (m * n) // gcd(m, n)
