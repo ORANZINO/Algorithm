1d8768e74b0512145b7467a326ecaeeadd0bb31f
ORANZINO <hupajupa61@gmail.com>
2021-03-11 23:48:30 +0900

    2021/03/11

---
+++ BOJ/11404.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+n = int(input())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    graph[i][i] = 0
+m = int(input())
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n + 1):
+    print(*[g if g != INF else 0 for g in graph[i][1:]])
