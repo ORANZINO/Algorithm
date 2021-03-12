511e8bb7ee14a51d087de17ab0bb8aa355da6a52
ORANZINO <hupajupa61@gmail.com>
2021-03-12 23:36:21 +0900

    2021/03/12

---
+++ BOJ/2887.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n = int(input())
+parent = list(range(n))
+x = []
+y = []
+z = []
+
+for i in range(n):
+    a, b, c = map(int, input().split())
+    x.append((a, i))
+    y.append((b, i))
+    z.append((c, i))
+x.sort()
+y.sort()
+z.sort()
+edges = []
+for i in range(n - 1):
+    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
+
+edges.sort()
+answer = 0
+
+for e in edges:
+    cost, a, b = e
+    if find_parent(a) != find_parent(b):
+        union_parent(a, b)
+        answer += cost
+print(answer)
