4592e61c5ba68e18a565201f645df45c105dff43
ORANZINO <hupajupa61@gmail.com>
2021-03-30 23:32:42 +0900

    2021/03/30

--- BOJ/2293.py
+++ BOJ/2293.py
@@ -4,9 +4,9 @@ ans = [0 for i in range(k + 1)]
 ans[0] = 1
 
 for i in arr:
-    for j in range(1, k + 1):
-        if j >= i:
-            ans[j] += ans[j - i]
-
+    j = i
+    while j <= k:
+        ans[j] += ans[j - i]
+        j += 1
 
 print(ans[k])
