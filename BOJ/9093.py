5611586d0306963fdbd601d12cccf8e926ef4a5e
ORANZINO <hupajupa61@gmail.com>
2021-04-18 16:33:29 +0900

    added some comments

--- BOJ/9093.py
+++ BOJ/9093.py
@@ -3,4 +3,5 @@ input = lambda : sys.stdin.readline().strip()
 
 t = int(input())
 for _ in range(t):
+    # 띄어쓰기로 각 어절 구분 후 뒤집어서 다시 붙여주기!
     print(' '.join(map(lambda x: ''.join(reversed(list(x))), input().split())))
