a9d69b0b5a143eaa02c59840278cbba5c6a77c23
ORANZINO <hupajupa61@gmail.com>
2021-05-07 23:16:23 +0900

    2021/05/07

--- BOJ/1002.py
+++ BOJ/1002.py
@@ -4,14 +4,14 @@ t = int(input())
 
 for _ in range(t):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    dist = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-    m = max(r1, r2, dist)
-    if m == (sum(r1, r2, dist) - m):
-        print(1)
-    elif dist == 0 and r1 == r2:
+    r = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    m = max(r1, r2, r)
+    rest = (r + r1 + r2) - m
+    if r == 0 and r1 == r2:
         print(-1)
-    elif m > (sum(r1, r2, dist) - m):
+    elif rest == m:
+        print(1)
+    elif m > rest:
         print(0)
     else:
         print(2)
-        
