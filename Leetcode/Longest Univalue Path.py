d50e16d5faaf55d4947f5e741561b74922887600
ORANZINO <hupajupa61@gmail.com>
2022-01-29 23:58:25 +0900

    2022/01/29

---
+++ Leetcode/Longest Univalue Path.py
@@ -0,0 +1,33 @@
+from typing import *
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    longest: int = 0
+
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if node.left and node.left.val == node.val:
+                left += 1
+            else:
+                left = 0
+            if node.right and node.right.val == node.val:
+                right += 1
+            else:
+                right = 0
+            self.longest = max(self.longest, left + right)
+            return max(left, right)
+
+        dfs(root)
+        return self.longest
+
