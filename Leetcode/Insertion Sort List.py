b39d57d9fb749e8d2fb0b0cb005a6977ec9daf53
ORANZINO <hupajupa61@gmail.com>
2022-02-26 23:54:40 +0900

    2022/02/26

--- Leetcode/Insertion Sort List.py
+++ Leetcode/Insertion Sort List.py
@@ -19,3 +19,11 @@ class Solution:
             if head and cur.val > head.val:
                 cur = parent
         return parent.next
+
+
+'''
+삽입 정렬을 사용하되, Linked List에서 순차탐색하며 삽입할 곳을 찾기 위해 
+계속 첫 엘리멘트를 가리키는 parent를 필요로 한다.
+다만 원래 큰 수에서부터 거꾸로 탐색하는 기존의 삽입 정렬 방식을 할 수 없으므로 효율성이 줄어든다.
+이에 따라 cur의 val이 head보다 커서 앞으로 돌아갈 필요성이 있을 때에만 처음으로 돌아가도록 하면 효율성을 높일 수 있다. 
+'''
--- Leetcode/Sort List.py
+++ Leetcode/Sort List.py
@@ -6,7 +6,30 @@ class ListNode:
         self.val = val
         self.next = next
 
-        
+# 병합정렬 사용하는 방법
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode):
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not (head and head.next):
+            return head
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+        head, slow = self.sortList(head), self.sortList(slow)
+        return self.mergeTwoLists(head, slow)
+
+
+# 내장 함수 사용하는 방법
+
+
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         p = head
@@ -23,3 +46,9 @@ class Solution:
             p = p.next
 
         return head
+
+
+'''
+1번 방법은 병합정렬 기법을 사용한다. Linked List를 반으로 나누기 위해 런너를 사용한다.
+2번 방법은 파이썬에 내장된 sort함수를 사용한다. 쉽고 직관적이지만 면접에서는 까일 것 같은 방법이랄까?
+'''
