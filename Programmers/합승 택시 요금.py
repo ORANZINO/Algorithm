fa9c6f7d3e6156e2b76ef315a2c1795da5644c14
ORANZINO <hupajupa61@gmail.com>
2022-04-18 14:31:25 +0900

    2022/04/18

--- Programmers/합승 택시 요금.py
+++ Programmers/합승 택시 요금.py
@@ -1,5 +1,5 @@
 def solution(n, s, a, b, fares):
-    answer = INF = int(1e9)
+    INF = int(1e9)
     graph = [[INF] * (n + 1) for _ in range(n + 1)]
     for i in range(n + 1):
         graph[i][i] = 0
@@ -9,9 +9,7 @@ def solution(n, s, a, b, fares):
     for k in range(1, n + 1):
         for i in range(1, n + 1):
             for j in range(1, n + 1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-    for i in range(1, n + 1):
-        answer = min(answer, graph[s][i] + graph[i][a] + graph[i][b])
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
 
-    return answer
-
+    return min(graph[s][i] + graph[i][a] + graph[i][b] for i in range(1, n + 1))
